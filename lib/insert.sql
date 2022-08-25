de1cc4dc9a3bcd241f617e8715315109461f8f5c
Vincent Langat <kimutailangat033@gmail.com>
2022-08-25 19:25:46 +0300

    commit tree 0ed7f40fdcb2e515663293b046d021bc0cbdd9d9

---
+++ lib/insert.sql
@@ -0,0 +1,24 @@
+INSERT INTO bears 
+(name, age, sex, color, temperament, alive) 
+VALUES ('Mr. Chocolate', 24, 'M', 'Red', 'Fury', 0);
+INSERT INTO bears
+(name, age, sex, color, temperament, alive) 
+VALUES ('Rowdy', 52, 'M', 'Green', 'Calm', 1);
+INSERT INTO bears 
+(name, age, sex, color, temperament, alive) 
+VALUES ('Tabitha', 32, 'F', 'Orange', 'Fury', 0);
+INSERT INTO bears
+(name, age, sex, color, temperament, alive) 
+VALUES ('Sergeant Brown', 31, 'M', 'Yellow', 'Fury', 0);
+INSERT INTO bears
+(name, age, sex, color, temperament, alive) 
+VALUES ('Mellisa', 28, 'M', 'Indigo', 'Fury', 1);
+INSERT INTO bears 
+(name, age, sex, color, temperament, alive) 
+VALUES ('Grinch', 24, 'M', 'Violet', 'Calm', 0);
+INSERT INTO bears 
+(name, age, sex, color, temperament, alive) 
+VALUES ('Wendy', 31, 'F', 'Blue', 'Dizzy', 0);
+INSERT INTO bears
+(name, age, sex, color, temperament, alive) 
+VALUES (NULL, 31, 'M', 'Yellow', 'Fury', 0);
